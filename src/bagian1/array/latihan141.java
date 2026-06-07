9eaa4f86bb74a470f31a65e3f2455da6aff44c1d
Istifarah Indrasasi Awal <istifarah71@gmail.com>
2026-06-07 14:04:16 +0800

    selesai bagian 1: Array

---
+++ src/bagian1/array/latihan141.java
@@ -0,0 +1,22 @@
+package bagian1.array;
+public class latihan141 {
+    public static void main(String[] args) {
+        double[] suhu = {30.5, 28.7, 31.2, 29.8, 32.1, 27.9};
+
+        double tertinggi = suhu[0];
+        double terendah = suhu[0];
+
+        for (int i = 1; i < suhu.length; i++) {
+            if (suhu[i] > tertinggi) {
+                tertinggi = suhu[i];
+            }
+
+            if (suhu[i] < terendah) {
+                terendah = suhu[i];
+            }
+        }
+
+        System.out.println("Suhu tertinggi: " + tertinggi);
+        System.out.println("Suhu terendah: " + terendah);
+    }
+}
---
+++ src/bagian1/array/latihan142.java
@@ -0,0 +1,20 @@
+package bagian1.array;
+public class latihan142 {
+    public static void main(String[] args) {
+    String[] hari = {
+        "Senin",
+        "Selasa",
+        "Rabu",
+        "Kamis",
+        "Jumat"
+    };
+
+    System.out.println("Nama hari dengan lebih dari 5 huruf:");
+
+     for (String h : hari) {
+     if (h.length() > 5) {
+      System.out.println(h);
+        }
+    }
+ }
+}
